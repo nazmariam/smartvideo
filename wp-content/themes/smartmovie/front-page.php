39c4ad705d242ea5f1e082e0defd7f82cb4c1fe6
Serhii Fedirko <serhii.fedirko@infopulse.com>
2019-04-08 13:35:26 +0300

    frontpage wip;

--- wp-content/themes/smartmovie/front-page.php
+++ wp-content/themes/smartmovie/front-page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<section class="section">
+<section class="section top">
     <video autoplay loop muted id="top_video">
         <source src="/wp-content/themes/smartmovie/Cloud-Landing-video-banner-1920.mp4" type="video/mp4">
         Your browser does not support HTML5 video.
@@ -14,6 +14,65 @@
                 Відео для бізнесу
             </a>
         </div>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <h2 class="section_title"><span>НАШИ ТАРИФЫ</span></h2>
+        <div class="tarifs">
+            <a href="#" class="tarif" style="background: url('http://lorempixel.com/400/400/cats/1') center no-repeat; background-size: cover">
+                <span class="tarif_title">
+                    Basic
+                </span>
+            </a>
+            <a href="#" class="tarif" style="background: url('http://lorempixel.com/400/400/cats/2') center no-repeat; background-size: cover">
+                <span class="tarif_title">
+                    Standart
+                </span>
+            </a>
+            <a href="#" class="tarif" style="background: url('http://lorempixel.com/400/400/cats/3') center no-repeat; background-size: cover">
+                <span class="tarif_title">
+                    Pro
+                </span>
+            </a>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <h2 class="section_title"><span>НАШИ РАБОТЫ</span></h2>
+        <div class="examples">
+            <a href="#" class="example" style="background: url('http://lorempixel.com/400/400/cats/1') center no-repeat; background-size: cover">
+            </a>
+            <a href="#" class="example" style="background: url('http://lorempixel.com/400/400/cats/2') center no-repeat; background-size: cover">
+            </a>
+            <a href="#" class="example" style="background: url('http://lorempixel.com/400/400/cats/3') center no-repeat; background-size: cover">
+            </a>
+            <a href="#" class="example" style="background: url('http://lorempixel.com/400/400/cats/4') center no-repeat; background-size: cover">
+            </a>
+            <a href="#" class="example" style="background: url('http://lorempixel.com/400/400/cats/5') center no-repeat; background-size: cover">
+            </a>
+            <a href="#" class="example" style="background: url('http://lorempixel.com/400/400/cats/6') center no-repeat; background-size: cover">
+            </a>
+            <a href="#" class="example" style="background: url('http://lorempixel.com/400/400/cats/7') center no-repeat; background-size: cover">
+            </a>
+            <a href="#" class="example" style="background: url('http://lorempixel.com/400/400/cats/8') center no-repeat; background-size: cover">
+            </a>
+            <a href="#" class="example" style="background: url('http://lorempixel.com/400/400/cats/9') center no-repeat; background-size: cover">
+            </a>
+        </div>
+    </div>
+</section>
+
+<section class="section last">
+    <video autoplay loop muted id="top_video">
+        <source src="/wp-content/themes/smartmovie/Cloud-Landing-video-banner-1920.mp4" type="video/mp4">
+        Your browser does not support HTML5 video.
+    </video>
+    <div class="container">
+        <h2>НАША ЦЕЛЬ — ПРИНОСИТЬ ВАМ НОВЫХ КЛИЕНТОВ И УВЕЛИЧИВАТЬ ВАШИ ПРОДАЖИ</h2>
     </div>
 </section>
 
